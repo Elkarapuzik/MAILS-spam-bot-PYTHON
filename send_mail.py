ff0a8e4cc7c951abc85995bb1cfacc5d4fdaf0d8
Elkarapuzik <shmakov-10@bk.ru>
2023-09-26 19:03:59 +0300

    Переработка функции чтоб отправлялось на все все почты

--- send_mail.py
+++ send_mail.py
@@ -1,16 +1,17 @@
 import smtplib
 
 def send_mail(text, mail, password, emails_to):
-  heading =f"""\
+  server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
+  server.login(mail, password)
+  for email in emails_to:
+    heading =f"""\
 From:{mail}
-To: {emails_to}
+To: {email}
 Subject: Реклама
 Content-Type: text/plain; charset="UTF-8";
 
-  """
-  letter = heading + text
-  letter = letter.encode("UTF-8")
-  server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
-  server.login(mail, password)
-  server.sendmail(mail, emails_to, letter)
+    """
+    letter = heading + text
+    letter = letter.encode("UTF-8")
+    server.sendmail(mail, email, letter)
   server.quit()
